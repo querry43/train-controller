af22b4eac42257a3a5233c817351a83f89184f7b
Matt Harrington <querry43+github@underdogma.net>
2016-01-31 11:09:54 -0800

    using ajaxy slider for speed control

--- software/webserver-control.lua
+++ software/webserver-control.lua
@@ -3,22 +3,26 @@ dofile('mcp4131.lua')
 TrainControl = {}
 TrainControl[1] = MCP4131:new({cs = 8})
 TrainControl[1]:set(0)
+TrainControl[1].val = 0
 
 return function (path, query)
-  if (query.speed == 'stop') then TrainControl[1]:set(0)
-  elseif (query.speed == 'slow') then TrainControl[1]:set(50)
-  elseif (query.speed == 'fast') then TrainControl[1]:set(100)
+  if(query.speed) then
+    TrainControl[1]:set(query.speed)
+    TrainControl[1].val = query.speed
   end
 
   return 200,
      '<html>'
+  ..   '<script type="text/javascript">'
+  ..     'function refresh() {'
+  ..       'var req = new XMLHttpRequest();'
+  ..       'req.open("GET", "/control?speed=" + document.getElementById("speed1").value, true);'
+  ..       'req.send();'
+  ..     '}'
+  ..   '</script>'
   ..   '<div style="text-align:center">'
-  ..     '<form method="get">'
-  ..       '<h2>Train Control</h2>'
-  ..       '<input type="submit" name="speed" value="stop">'
-  ..       '<input type="submit" name="speed" value="slow">'
-  ..       '<input type="submit" name="speed" value="fast">'
-  ..     '</form>'
+  ..     '<h2>Train Control</h2>'
+  ..     '<input type="range" id="speed1" min="0" max="100" step="10" onchange="refresh()" value="' .. TrainControl[1].val .. '">'
   ..   '</div>'
   .. '</html>'
 end
