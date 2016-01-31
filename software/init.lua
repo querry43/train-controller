e00a4d6a500a12c08d41990294730a984febfa44
Matt Harrington <querry43+github@underdogma.net>
2016-01-30 21:24:39 -0800

    reducing memory footprint

--- software/init.lua
+++
@@ -1,6 +0,0 @@
-dofile('network.lua')
-Network:load()
-
-dofile('webserver.lua')()
-dofile('webserver-status.lua')
-dofile('webserver-network-config.lua')
--- software/load-all.lua
+++ software/load-all.lua
@@ -3,8 +3,9 @@ Network:load()
 
 dofile('webserver.lua')()
 
-WebServerPages['^/network'] = dofile('webserver-network.lua')
-WebServerPages['^/status'] = dofile('webserver-status.lua')
-WebServerPages['^/$'] = dofile('webserver-frame.lua')
-WebServerPages['^/menu'] = dofile('webserver-menu.lua')
+WebServerPages['^/network'] = function(path, query) return dofile('webserver-network.lua')(path, query) end
+WebServerPages['^/status'] = function(path, query) return dofile('webserver-status.lua')(path, query)  end
+WebServerPages['^/$'] = function(path, query) return dofile('webserver-frame.lua')(path, query)  end
+WebServerPages['^/menu'] = function(path, query) return dofile('webserver-menu.lua')(path, query)  end
+
 WebServerPages['^/control'] = dofile('webserver-control.lua')
