556fc7811324c0794b681c774638a8cf3b23f2e1
Matt Harrington <querry43+github@underdogma.net>
2016-01-31 15:08:12 -0800

    improving defaults

--- software/load-all.lua
+++ software/load-all.lua
@@ -1,7 +1,12 @@
 dofile('network.lua')
+
+Network.ap_ssid = 'train control tower ' .. node.chipid();
+
 Network:load()
 
 dofile('webserver.lua')()
+
+content_path = '/control'
 
 WebServerPages['^/network'] = function(path, query) return dofile('webserver-network.lua')(path, query) end
 WebServerPages['^/status'] = function(path, query) return dofile('webserver-status.lua')(path, query)  end
