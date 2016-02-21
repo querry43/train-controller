5f53c21ba019b1ed43ef056b281c3b658ad20bbf
Matt Harrington <querry43@gmail.com>
2016-02-20 21:43:58 -0800

    slider improvements

--- software/webserver-control.lua
+++ software/webserver-control.lua
@@ -7,16 +7,16 @@ TrainControl[2] = MCP4131:new({cs = 2})
 TrainControl[3] = MCP4131:new({cs = 4})
 
 for i=1,3 do
+  TrainControl[i]:set(0)
   TrainControl[i].val = 0
-  TrainControl[i].reverse = false
 end
 
 return function (path, query)
   if(query.speed1) then
     for i=1,3 do
-      TrainControl[i]:set(query['speed' .. i])
-      TrainControl[i].val = query['speed' .. i]
-      TrainControl[i].reverse = query['rev' .. i] == 'true'
+      local s = query['speed' .. i]
+      TrainControl[i]:set(100 - math.abs(s))
+      TrainControl[i].val = s
     end
   end
 
@@ -24,17 +24,14 @@ return function (path, query)
      '<html>'
   ..   '<div style="text-align:center">'
   ..     '<h2>Train Control</h2>'
-  ..     '<input type="range" id="speed1" min="0" max="100" step="10" onchange="refresh()" value="' .. TrainControl[1].val .. '">'
-  ..     '<input type="text" id="speed1_val" size="1" disabled value="' .. TrainControl[1].val .. '%">'
-  ..     '<input type="checkbox" id="rev1" onchange="refresh()" ' .. (TrainControl[1].reverse and 'checked' or '') .. '>Reverse'
+  ..     '<input type="range" id="speed1" min="-100" max="100" step="10" style="width: 50%;" onchange="refresh()" value="' .. TrainControl[1].val .. '">'
+  ..     '<output id="speed1_val" for="speed1"></output>'
   ..     '<br />'
-  ..     '<input type="range" id="speed2" min="0" max="100" step="10" onchange="refresh()" value="' .. TrainControl[2].val .. '">'
-  ..     '<input type="text" id="speed2_val" size="1" disabled value="' .. TrainControl[2].val .. '%">'
-  ..     '<input type="checkbox" id="rev2" onchange="refresh()" ' .. (TrainControl[2].reverse and 'checked' or '') .. '>Reverse'
+  ..     '<input type="range" id="speed2" min="-100" max="100" step="10" style="width: 50%;" onchange="refresh()" value="' .. TrainControl[2].val .. '">'
+  ..     '<output id="speed2_val" for="speed2"></output>'
   ..     '<br />'
-  ..     '<input type="range" id="speed3" min="0" max="100" step="10" onchange="refresh()" value="' .. TrainControl[3].val .. '">'
-  ..     '<input type="text" id="speed3_val" size="1" disabled value="' .. TrainControl[3].val .. '%">'
-  ..     '<input type="checkbox" id="rev3" onchange="refresh()" ' .. (TrainControl[3].reverse and 'checked' or '') .. '>Reverse'
+  ..     '<input type="range" id="speed3" min="-100" max="100" step="10" style="width: 50%;" onchange="refresh()" value="' .. TrainControl[3].val .. '">'
+  ..     '<output id="speed3_val" for="speed3"></output>'
   ..   '</div>'
   ..   '<script type="text/javascript">'
   ..     'function refresh() {'
@@ -43,12 +40,12 @@ return function (path, query)
   ..         'var speed = document.getElementById("speed" + i).value;'
   ..         'document.getElementById("speed" + i + "_val").value = speed + "%";'
   ..         'queryParams.push("speed" + i + "=" + speed);'
-  ..         'queryParams.push("rev" + i + "=" + document.getElementById("rev" + i).checked);'
   ..       '}'
   ..       'var req = new XMLHttpRequest();'
   ..       'req.open("GET", "/control?" + queryParams.join("&"), true);'
   ..       'req.send();'
   ..     '}'
+  ..     'refresh();'
   ..   '</script>'
   .. '</html>'
 end
