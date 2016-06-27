37ada8c73f5611726c5bc9308d7d8e6faf6ed094
Matt Harrington <querry43+github@underdogma.net>
2016-06-26 21:44:59 -0700

    arduino support for multiple lines

--- software/arduino/arduino.ino
+++ software/arduino/arduino.ino
@@ -1,23 +1,59 @@
 #include <SPI.h>
 #include <McpDigitalPot.h>
 
-const uint8_t ss = 8;
-const uint8_t dir = 9;
 const uint8_t maxbuflen = 16;
 const int8_t acceleration = 2;
 
-McpDigitalPot digitalPot = McpDigitalPot(ss, 10000);
+class WhistleBoard {
+public:
+  WhistleBoard(const uint8_t ss, const uint8_t dir) : _speed(0), _target_speed(0), _dir(dir), _digital_pot(ss, 10000) {
+    pinMode(_dir, OUTPUT);
+  }
 
-int8_t speed, target_speed;
+  void set_target_speed(int8_t target_speed) { _target_speed = target_speed; }
+
+  void halt() {
+    _speed = 0;
+    _target_speed = 0;
+    _update_digipot();
+  }
+
+  void update() {
+    if (_speed > _target_speed) {
+      _speed = max(_speed - acceleration, _target_speed);
+      _update_digipot();
+    } else if (_speed < _target_speed) {
+      _speed = min(_speed + acceleration, _target_speed);
+      _update_digipot();
+    }
+  }
+ 
+private:
+  int8_t _speed, _target_speed;
+  uint8_t _dir;
+  McpDigitalPot _digital_pot;
+
+  WhistleBoard();
+
+  void _update_digipot() {
+    if (_speed >= 0) digitalWrite(_dir, LOW);
+    else             digitalWrite(_dir, HIGH);
+
+    _digital_pot.setResistance(0, map(abs(_speed), 0, 100, 10000, 0));
+
+    Serial.print("SPEED: ");
+    Serial.println(_speed);
+    delay(100);
+  }
+};
+
+WhistleBoard wbs[] = {
+  WhistleBoard(8, 9),
+  WhistleBoard(6, 7),
+};
 
 void setup() {
-  pinMode(dir, OUTPUT);
   SPI.begin();
-
-  speed = 0;
-  target_speed = 0;
-
-  set_speed(0);
 
   Serial.begin(9600);
   Serial.println("OK");
@@ -29,13 +65,8 @@ void loop() {
     run_command(command);
   }
 
-  if (speed > target_speed) {
-    speed = max(speed - acceleration, target_speed);
-    set_speed(speed);
-  } else if (speed < target_speed) {
-    speed = min(speed + acceleration, target_speed);
-    set_speed(speed);
-  }
+  wbs[0].update();
+  wbs[1].update();
 }
 
 bool read_command(char* command) {
@@ -71,31 +102,44 @@ bool read_command(char* command) {
 
 void run_command(char* command) {
   char p = tolower(command[0]);
-  int32_t v = strtol(command+1, NULL, 10);
+  uint8_t i = command[1] - '0';
+  int32_t v = strtol(command+2, NULL, 10);
+
+
 
   switch (p) {
     case '?':
-      Serial.println("G<speed> - Go, -100 to 100");
-      Serial.println("S - Stop");
+      Serial.println("G<i><speed> - Go, -100 to 100");
+      Serial.println("S<i> - Stop");
       Serial.println("H - Immediate halt");
       break;
 
     case 's':
-      target_speed = 0;
-      Serial.println("STOP OK");
+      if (i >= 2) {
+        Serial.println("NOT OK: Attempt to index > 2");
+      } else {
+        wbs[i].set_target_speed(0);
+        Serial.print("STOP OK: ");
+        Serial.println(i);
+      }
       break;
 
     case 'g':
-      target_speed = v;
-      Serial.print("GO OK: ");
-      Serial.println(v);
+      if (i >= 2) {
+        Serial.println("NOT OK: Attempt to index > 2");
+      } else {
+        wbs[i].set_target_speed(v);
+        Serial.print("GO OK: ");
+        Serial.print(i);
+        Serial.print(" => ");
+        Serial.println(v);
+      }
       break;
 
     case 'h':
-      target_speed = 0;
-      speed = 0;
+      wbs[0].halt();
+      wbs[1].halt();
       Serial.println("HALT OK");
-      set_speed(0);
       break;
 
     default:
@@ -103,15 +147,4 @@ void run_command(char* command) {
       Serial.println(p);
       break;
   }
-}
-
-void set_speed(int8_t s) {
-  if (s >= 0) digitalWrite(dir, LOW);
-  else        digitalWrite(dir, HIGH);
-
-  digitalPot.setResistance(0, map(abs(s), 0, 100, 10000, 0));
-
-  Serial.print("SPEED: ");
-  Serial.println(speed);
-  delay(100);
 }
